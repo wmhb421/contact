6e911092e20d4472e66fa84d7258567f5a18b708
koko-295-529 <2872331797@qq.com>
2024-11-24 16:18:20 +0800

    Add files via upload

---
+++ 导出/demo/src/main/java/com/example/common/WebMvcConfig.java
@@ -0,0 +1,22 @@
+package com.example.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 832202116Yike Chen
+ * date： 2024/10/26
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedHeaders("Content-Type","X-Requested-With","accept,Origin","Access-Control-Request-Method","Access-Control-Request-Headers","token")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .allowCredentials(true);
+    }
+}
